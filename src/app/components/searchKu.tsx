a87acf7a8d4b848875107d7473ee316ee79dd925
ChrisFletcher1989 <chrisfletcher1989@gmail.com>
2024-01-06 17:55:42 +0900

    initial commit, just the skeleton for now

---
+++ src/app/components/searchKu.tsx
@@ -0,0 +1,55 @@
+export default function SearchKu() {
+  return (
+    <div>
+      <p className="text-center">Search by Tokyo Ward:</p>
+      <form className="text-base font-semibold leading-7 text-gray-900 text-center">
+        <input type="checkbox" name="adachi" id="adachi"></input>
+        <label className="mr-4">Adachi</label>
+        <input type="checkbox" name="arakawa" id="arakawa"></input>
+        <label className="mr-4">Arakawa</label>
+        <input type="checkbox" name="bunkyo" id="bunkyo"></input>
+        <label className="mr-4">Bunkyo</label>
+        <input type="checkbox" name="chiyoda" id="chiyoda"></input>
+        <label className="mr-4">Chiyoda</label>
+        <input type="checkbox" name="chuo" id="chuo"></input>
+        <label className="mr-4">Chuo</label>
+        <input type="checkbox" name="edogawa" id="edogawa"></input>
+        <label className="mr-4">Edogawa</label>
+        <input type="checkbox" name="itabashi" id="itabashi"></input>
+        <label className="mr-4">Itabashi</label>
+        <input type="checkbox" name="katsushika" id="katsushika"></input>
+        <label className="mr-4">Katsushika</label>
+        <input type="checkbox" name="kita" id="kita"></input>
+        <label className="mr-4">Kita</label>
+        <input type="checkbox" name="koto" id="koto"></input>
+        <label className="mr-4">Koto</label>
+        <input type="checkbox" name="minato" id="minato"></input>
+        <label className="mr-4">Minato</label>
+        <input type="checkbox" name="meguro" id="meguro"></input>
+        <label className="mr-4">Meguro</label>
+        <input type="checkbox" name="nakano" id="nakano"></input>
+        <label className="mr-4">Nakano</label>
+        <input type="checkbox" name="nerima" id="nerima"></input>
+        <label className="mr-4">Nerima</label>
+        <input type="checkbox" name="ota" id="ota"></input>
+        <label className="mr-4">Ota</label>
+        <input type="checkbox" name="setagaya" id="setagaya"></input>
+        <label className="mr-4">Setagaya</label>
+        <input type="checkbox" name="shibuya" id="shibuya"></input>
+        <label className="mr-4">Shibuya</label>
+        <input type="checkbox" name="shinagawa" id="shinagawa"></input>
+        <label className="mr-4">Shinagawa</label>
+        <input type="checkbox" name="shinjuku" id="shinjuku"></input>
+        <label className="mr-4">Shinjuku</label>
+        <input type="checkbox" name="suginami" id="suginami"></input>
+        <label className="mr-4">Suginami</label>
+        <input type="checkbox" name="sumida" id="sumida"></input>
+        <label className="mr-4">Sumida</label>
+        <input type="checkbox" name="taito" id="taito"></input>
+        <label className="mr-4">Taito</label>
+        <input type="checkbox" name="toshima" id="toshima"></input>
+        <label className="mr-4">Toshima</label>
+      </form>
+    </div>
+  );
+}
